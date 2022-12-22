b01e83553d5090a56e014ca15466b3be1a79427b
akshu1 <akshathanagaraj98@gmail.com>
2022-12-22 15:37:15 +0530

    Add:Program to check whether the user has entered valid LastName

--- src/main/java/com/bridgelabz/regex/UserInputValidation.java
+++ src/main/java/com/bridgelabz/regex/UserInputValidation.java
@@ -12,16 +12,37 @@ import java.util.regex.Matcher;
             Matcher matcher = pattern.matcher(firstName);
 
             return matcher.matches();
+        }
+        public static boolean isValidLastName(String lastName) {
+            String firstNameRegex = "[A-Z]{1}[a-z]{2,}";
 
+            Pattern pattern = Pattern.compile(firstNameRegex);
 
+            Matcher matcher = pattern.matcher(lastName);
+
+            return matcher.matches();
         }
 
         public static void main(String[] args) {
-            UserInputValidation userInputValidation = new UserInputValidation();
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter first name: ");
-            String firstName = scanner.next();
-            System.out.println(UserInputValidation.isValidFirstName(firstName) ? "Valid" : "In Valid");
+            Scanner scan = new Scanner(System.in);
+            // Method To Check First Name With Regex Start With Cap And 3 Min. Character
+            System.out.println("Enter the first Name: ");
+            String firstName = scan.nextLine();
+            System.out.println("Enter the last Name :");
+            String lastName = scan.nextLine();
+
+            if (Pattern.matches("^[A-Z][a-z]{2,}", firstName)) {
+                System.out.println("First name is valid");
+            } else {
+                System.out.println("Entered first name is invalid");
+            }
+            scan.close();
+            // Method To Check Last Name With Regex Start With Cap And 3 Min. Character
+            if (Pattern.matches("^[A-Z][a-z]{2,}", lastName)) {
+                System.out.println("Last name is valid");
+            } else {
+                System.out.println("Entered last name is invalid");
+            }
         }
     }
 
